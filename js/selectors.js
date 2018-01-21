0bef54b6b7861310f25f965cf3cb215467167130
Grace Shih <vocalise@gmail.com>
2018-01-21 17:41:53 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+$(document).ready(function() {
+
+})
+
+  function paragraphSelector() {
+    var p_tags = $('p');
+    return p_tags;
+  }
+  
+  function lastImageSelector() {
+    var images = $('.pics img');
+    var lastImage = images.last();
+    return lastImage;
+  }
+  
+  function ninjaBabySelector() {
+    
+  }
+  
+  function divSelector() {
+    
+  }
